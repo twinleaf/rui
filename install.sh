eddd0be224c6c8bfb92210b441ec50575e5aa84e
Christopher Zhao <zhao@twinleaf.com>
2026-02-17 05:24:10 -0500

    install.sh: don't re-initialize venv and correctly source alias in current shell with source install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
-python3 -m venv $HOME/python
+# source install.sh to set up rui for bash or zsh
+# currently no support for any other shell that don't conveniently use ~/.*shrc
+
+if ! [ -d $HOME/python ]; then
+	python3 -m venv $HOME/python
+fi
 $HOME/python/bin/pip install twinleaf
 $HOME/python/bin/pip install pyqt6
 
-rui_alias="alias rui=\"$HOME/python/bin/python $( pwd )/rui.py\""
+rui_alias="$HOME/python/bin/python $( pwd )/rui.py"
 rc="$HOME/.$( ps cp "$$" -o command= )rc"
-if ! cat "$rc" | grep -qe "^$rui_alias$"; then
+if ! cat "$rc" | grep -qe "$rui_alias"; then
 	cat << EOF >> "$rc"
 
 # >>>>> RUI >>>>>
 # !! Contents within this block are managed by RUI
-$rui_alias
+alias rui="$rui_alias"
 # <<<<< RUI <<<<<
 EOF
 fi
+
+# enable alias in this shell
+alias rui="$rui_alias"
